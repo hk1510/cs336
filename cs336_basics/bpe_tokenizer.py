24ad89547307a3400b33c76b84d41668785d0333
Harshil Kotamreddy <hkthetinkerer@gmail.com>
2026-03-16 12:57:27 -0700

    cs336 basics

---
+++ cs336_basics/bpe_tokenizer.py
@@ -0,0 +1,34 @@
+import regex as re
+from typing import BinaryIO
+from .pretokenization_example import find_chunk_boundaries
+
+class BPETokenizer:
+    
+    def __init__(self, vocab_size: int, special_tokens: list[str]):
+
+        self.vocab = special_tokens + [bytes([i]) for i in range(256)]
+
+        min_vocab_size = len(self.vocab)
+
+        if vocab_size <= min_vocab_size:
+            raise ValueError(f"Vocab size is less than the minimum of 256 + {len(special_tokens)} = {min_vocab_size}")
+
+        self.vocab_size = vocab_size
+
+        self.pretokenize_pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""" 
+
+    def tokenize(self, file_path: str):
+        
+        with open(file_path, "rb") as f:
+            num_processes = 4
+            boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+
+            # The following is a serial implementation, but you can parallelize this
+            # by sending each start/end pair to a set of processes.
+            for start, end in zip(boundaries[:-1], boundaries[1:]):
+                f.seek(start)
+                chunk = f.read(end - start).decode("utf-8", errors="ignore")
+                # Run pre-tokenization on your chunk and store the counts for each pre-token
+                for pretok in re.finditer(self.pretokenize_pattern, chunk):
+
+        
